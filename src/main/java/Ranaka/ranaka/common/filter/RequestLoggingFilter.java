bf470f36793678d7367aa28fb06b1c4ce4b6a8f8
Stile-1473 <stilesmvura@gmail.com>
2026-05-03 15:42:35 +0200

    Logger

---
+++ src/main/java/Ranaka/ranaka/common/filter/RequestLoggingFilter.java
@@ -0,0 +1,40 @@
+package Ranaka.ranaka.common.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        long start = System.currentTimeMillis();
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            long durationMs = System.currentTimeMillis() - start;
+            log.info(
+                    "{} {} -> {} ({} ms) origin={}",
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    response.getStatus(),
+                    durationMs,
+                    request.getHeader("Origin")
+            );
+        }
+    }
+}
